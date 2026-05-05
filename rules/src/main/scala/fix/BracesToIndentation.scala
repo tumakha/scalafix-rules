2ca24bc51a9100d1742071dd706c3abd73329386
Yuriy Tumakha <tumakha@gmail.com>
2026-05-05 19:33:58 +0100

    BracesToIndentation

--- rules/src/main/scala/fix/BracesToIndentation.scala
+++ rules/src/main/scala/fix/BracesToIndentation.scala
@@ -7,89 +7,40 @@ import scala.meta._
 /**
  * @author Yuriy Tumakha
  */
-class BracesToIndentation extends SyntacticRule("BracesToIndentation") {
+class BracesToIndentation extends SemanticRule("BracesToIndentation") {
 
-  override def fix(implicit doc: SyntacticDocument): Patch = {
+  override def fix(implicit doc: SemanticDocument): Patch =
     doc.tree.collect {
-      case cls: Defn.Class  => transformClass(cls)
-      case trt: Defn.Trait  => transformTrait(trt)
-      case obj: Defn.Object => transformObject(obj)
+      case cls: Defn.Class  => rewrite(cls)
+      case trt: Defn.Trait  => rewrite(trt)
+      case obj: Defn.Object => rewrite(obj)
     }.asPatch
-  }
 
-  // ---------- CLASS ----------
-  private def transformClass(cls: Defn.Class): Patch = {
-    val header =
-      s"${renderMods(cls.mods)}class ${cls.name.value}" +
-        renderTParams(cls.tparams) +
-        renderCtor(cls.ctor) +
-        renderParents(cls.templ)
-
-    val body = renderBody(cls.templ)
-
-    Patch.replaceTree(cls, header + body)
-  }
-
-  // ---------- TRAIT ----------
-  private def transformTrait(trt: Defn.Trait): Patch = {
-    val header =
-      s"${renderMods(trt.mods)}trait ${trt.name.value}" +
-        renderTParams(trt.tparams) +
-        renderParents(trt.templ)
-
-    val body = renderBody(trt.templ)
-
-    Patch.replaceTree(trt, header + body)
-  }
-
-  // ---------- OBJECT ----------
-  private def transformObject(obj: Defn.Object): Patch = {
-    val header =
-      s"${renderMods(obj.mods)}object ${obj.name.value}" +
-        renderParents(obj.templ)
-
-    val body = renderBody(obj.templ)
-
-    Patch.replaceTree(obj, header + body)
-  }
-
-  // ---------- HELPERS ----------
-
-  private def renderMods(mods: List[Mod]): String =
-    if (mods.isEmpty) "" else mods.map(_.syntax).mkString("", " ", " ")
-
-  private def renderTParams(tparams: List[Type.Param]): String =
-    if (tparams.isEmpty) ""
-    else tparams.map(_.syntax).mkString("[", ", ", "]")
-
-  private def renderCtor(ctor: Ctor.Primary): String =
-    renderParamss(ctor.paramss)
-
-  private def renderParamss(paramss: List[List[Term.Param]]): String =
-    paramss
-      .map(params => params.map(_.syntax).mkString("(", ", ", ")"))
-      .mkString("")
-
-  private def renderParents(templ: Template): String = {
-    templ.parent match {
-      case None => ""
-
-      case Some(init) =>
-        // init has: ctor + args + type
-        val first = init.syntax
-
-        init match {
-          case _ =>
-            s" extends $first"
-        }
+  private def rewrite(defn: Tree): Patch = {
+    val templ = defn match {
+      case c: Defn.Class  => c.templ
+      case t: Defn.Trait  => t.templ
+      case o: Defn.Object => o.templ
     }
+
+    val tokens = templ.tokens
+
+    val openBrace  = tokens.find(_.is[Token.LeftBrace])
+    val closeBrace = tokens.find(_.is[Token.RightBrace])
+
+    val patches = List.newBuilder[Patch]
+
+    // 1. Replace `{` with `:`
+    openBrace.foreach { t =>
+      patches += Patch.replaceToken(t, ":")
+    }
+
+    // 2. Remove `}` completely (only structural brace)
+    closeBrace.foreach { t =>
+      patches += Patch.removeToken(t)
+    }
+
+    patches.result().asPatch
   }
 
-  private def renderBody(templ: Template): String = {
-    val body = templ.stats.map(s => indent(s.syntax)).mkString("\n")
-    s":\n$body"
-  }
-
-  private def indent(code: String): String =
-    code.linesIterator.map("  " + _).mkString("\n")
 }
